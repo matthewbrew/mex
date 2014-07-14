bd5bb7ce4311f0689ecc642008c8bfd6b0cc28b3
matthewbrew <matthew.brew@netregistry.com.au>
2014-07-14 16:10:29 +1000

    extra changes

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -47,7 +47,7 @@ namespace Exchange
 
                 Random rnd = new Random();
                 int randomUserNumber = rnd.Next(100000); 
-                adUser.setUserPrincipalName(randomUserNumber + "newusertest@cust1.exchtest.webcentral.com.au");
+                adUser.setIdentity(randomUserNumber + "newusertest@cust1.exchtest.webcentral.com.au");
                 adUser.setAccountPassword(randomUserNumber + "Test#");
                 adUser.setCustomerID("vs000013");
                 log.Info(localExchange.SetMtAduser(adUser));
